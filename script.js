b561f0ae71880452015a8455c8a205201fb8454b
dennisomar <dmogrovejo@gmail.com>
2025-10-03 11:05:06 -0500

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(){
-  fetch('https://api.countapi.xyz/hit/lechedecabraweb/visitas')
-  .then(res => res.json())
-  .then(res => {
-    document.getElementById('counter-number').innerText = res.value;
-  });
-});
